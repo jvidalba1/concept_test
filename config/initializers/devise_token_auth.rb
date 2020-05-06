fe0f427d09ccb6a8f78f04339e3f589200bf6f02
Mateo Vidal <jvidalba@gmail.com>
2020-05-06 09:45:27 -0500

    Add missing configuration and locale definitions

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -11,6 +11,8 @@ DeviseTokenAuth.setup do |config|
   # determines how long tokens will remain valid after they are issued.
   # config.token_lifespan = 2.weeks
 
+  config.default_password_reset_url = "localhost:3000"
+
   # Limiting the token_cost to just 4 in testing will increase the performance of
   # your test suite dramatically. The possible cost value is within range from 4
   # to 31. It is recommended to not use a value more than 10 in other environments.
